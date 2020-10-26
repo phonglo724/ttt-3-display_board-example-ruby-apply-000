f266450dbdaf75fca0688a7b6ca6bb4cda4dd28e
Phong Lo <phonglo724@gmail.com>
2020-10-26 23:48:14 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,20 @@
+Welcome to Tic Tac Toe
+   |   |  
+-----------
    |   |   
 -----------
    |   |   
+What move would X like to make (1-9)?
+> 5
+   |   |   
+-----------
+   | X |   
+-----------
+   |   |   
+What move would O like to make (1-9)?
+> 1
+ O |   |   
+-----------
+   | X |   
 -----------
    |   |   
