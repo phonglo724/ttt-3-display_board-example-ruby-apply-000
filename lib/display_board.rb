9864b6f4b8ceb1c4dd03ca9d0dd50c3f3d1dddcd
Phong Lo <phonglo724@gmail.com>
2020-10-27 00:19:18 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,10 @@
-Welcome to Tic Tac Toe
-   |   |  
------------
-   |   |   
------------
-   |   |   
+def display_board
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
 What move would X like to make (1-9)?
 > 5
    |   |   
@@ -18,3 +19,45 @@ What move would O like to make (1-9)?
    | X |   
 -----------
    |   |   
+What move would X like to make (1-9)?
+> 2
+ O | X |   
+-----------
+   | X |   
+-----------
+   |   |   
+What move would O like to make (1-9)?
+> 8
+ O | X |   
+-----------
+   | X |   
+-----------
+   | O |   
+What move would X like to make (1-9)?
+> 3
+ O | X | X   
+-----------
+   | X |   
+-----------
+   | O |   
+What move would O like to make (1-9)?
+> 7
+ O | X | X   
+-----------
+   | X |   
+-----------
+ O | O |   
+What move would X like to make (1-9)?
+> 4
+ O | X | X   
+-----------
+ X | X |   
+-----------
+ O | O |   
+ What move would O like to make (1-9)?
+ > 9
+ O | X | X   
+-----------
+ X | X |   
+-----------
+ O | O | O   
